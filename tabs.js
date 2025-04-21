8875f7be629a34c8519b6e9dde6c96b48cf0f47b
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-04-21 18:32:06 +0200

    Refactoring the code in tabs

--- tabs.js
+++ tabs.js
@@ -18,20 +18,22 @@ function changeTabFocus(event) {
   // change the tabindex of the current tab to -1
   if (event.keyCode === keydownLeft || event.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
-  // if the right key is pushed, move to the next tab on the right
-  if (event.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) tabFocus = 0;
-  }
-  // if the left key is pushed, move to the next tab on the left
-  if (event.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) tabFocus = tabs.length - 1;
-  }
 
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
+    // if the right key is pushed, move to the next tab on the right
+    if (event.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) tabFocus = 0;
+    }
+
+    // if the left key is pushed, move to the next tab on the left
+    if (event.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) tabFocus = tabs.length - 1;
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+  }
 }
 
 function changeTabPannel(event) {
@@ -43,18 +45,36 @@ function changeTabPannel(event) {
 
   // Hiding the previous tab
   // Article
-  mainContainer
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel) => panel.setAttribute("hidden", true));
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden"); // ex: #mars-tab
+  //   mainContainer
+  //     .querySelectorAll('[role="tabpanel"]')
+  //     .forEach((panel) => panel.setAttribute("hidden", true));
+  //   mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden"); // ex: #mars-tab
+
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, [`#${targetPanel}`]);
 
   // Image
-  mainContainer
-    .querySelectorAll("picture")
-    .forEach((picture) => picture.setAttribute("hidden", true));
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+  //   mainContainer
+  //     .querySelectorAll("picture")
+  //     .forEach((picture) => picture.setAttribute("hidden", true));
+  //   mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+
+  hideContent(mainContainer, "picture");
+  showContent(mainContainer, [`#${targetImage}`]);
 
   // active tag
-  tabContainer.querySelector('[aria-selected="true"]').setAttribute("aria-selected", false);
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
   targetTab.setAttribute("aria-selected", true);
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden"); // ex: #mars-tab
 }
