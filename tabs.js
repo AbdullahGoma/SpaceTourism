7a6bf07ab09f6f3cf2352b255808fdb15fb1a7f0
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-04-21 17:06:11 +0200

    Change the tab content when click

--- tabs.js
+++ tabs.js
@@ -3,27 +3,41 @@ const tabs = tabList.querySelectorAll('[role="tab"]');
 
 tabList.addEventListener("keydown", changeTabFocus);
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPannel);
+});
+
 let tabFocus = 0;
 
 function changeTabFocus(event) {
-    const keydownLeft = 37;
-    const keydownRight = 39;
+  const keydownLeft = 37;
+  const keydownRight = 39;
 
-    // change the tabindex of the current tab to -1
-    if (event.keyCode === keydownLeft || event.keyCode === keydownRight) {
-      tabs[tabFocus].setAttribute("tabindex", -1);
-    }
-    // if the right key is pushed, move to the next tab on the right
-    if (event.keyCode === keydownRight) {
-      tabFocus++;
-      if (tabFocus >= tabs.length) tabFocus = 0;
-    }
-    // if the left key is pushed, move to the next tab on the left
-    if (event.keyCode === keydownLeft) {
-      tabFocus--;
-      if (tabFocus < 0) tabFocus = tabs.length - 1;
-    }
+  // change the tabindex of the current tab to -1
+  if (event.keyCode === keydownLeft || event.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+  // if the right key is pushed, move to the next tab on the right
+  if (event.keyCode === keydownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) tabFocus = 0;
+  }
+  // if the left key is pushed, move to the next tab on the left
+  if (event.keyCode === keydownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) tabFocus = tabs.length - 1;
+  }
 
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
+
+function changeTabPannel(event) {
+    const targetTab = event.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+
+    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    
 }
